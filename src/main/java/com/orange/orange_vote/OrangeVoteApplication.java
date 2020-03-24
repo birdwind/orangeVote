82ced03a08b937fa2ff147c5c0f72579577830dc
birdwind <birdwind0105@gmail.com>
2020-03-24 18:46:07 +0800

    初始化

---
+++ src/main/java/com/orange/orange_vote/OrangeVoteApplication.java
@@ -0,0 +1,13 @@
+package com.orange.orange_vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeVoteApplication.class, args);
+	}
+
+}
